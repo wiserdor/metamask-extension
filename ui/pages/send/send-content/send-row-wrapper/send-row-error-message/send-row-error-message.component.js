eeff455c64501ce88a4b5c487382828566bcf8ce
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-05-28 17:38:01 +0400

    Styles for hex data
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/send/send-content/send-row-wrapper/send-row-error-message/send-row-error-message.component.js
+++ ui/pages/send/send-content/send-row-wrapper/send-row-error-message/send-row-error-message.component.js
@@ -46,6 +46,7 @@ export default class SendRowErrorMessage extends Component {
       <div
         className={classnames('send-v2__error', {
           'send-v2__error-amount': errorType === 'amount',
+          'send-v2__error-hex-data': errorType === 'hexData',
         })}
       >
         {errorMessage}
