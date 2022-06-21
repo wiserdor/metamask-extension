e56d0be9f422ddbaf341f4edc348b601a323769b
Alex <adonesky@gmail.com>
2022-06-21 13:03:15 -0500

    fix ignoreTokens

--- ui/pages/add-collectible/add-collectible.js
+++ ui/pages/add-collectible/add-collectible.js
@@ -66,7 +66,10 @@ export default function AddCollectible() {
     }
     if (contractAddressToConvertFromTokenToCollectible) {
       await dispatch(
-        ignoreTokens(contractAddressToConvertFromTokenToCollectible),
+        ignoreTokens({
+          tokensToIgnore: contractAddressToConvertFromTokenToCollectible,
+          dontShowLoadingIndicator: true,
+        }),
       );
     }
     dispatch(setNewCollectibleAddedMessage('success'));
